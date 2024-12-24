856a04519e6993370ef18fbe9532433b7436af50
SonRattana <rattanasgcc210013@fpt.edu.vn>
2024-12-25 00:21:06 +0700

    test

--- data/books.js
+++ data/books.js
@@ -1,4 +1,76 @@
 module.exports = [
-    { isbn: "12345", title: "Book One", author: "Author A", review: "Good book!" },
-    { isbn: "67890", title: "Book Two", author: "Author B", review: "Excellent read!" }
+    { 
+        isbn: "12345", 
+        title: "Book One", 
+        author: "Author A", 
+        review: "Good book!" 
+      },
+      { 
+        isbn: "67890", 
+        title: "Book Two", 
+        author: "Author B", 
+        review: "Excellent read!" 
+      },
+      { 
+        isbn: "11223", 
+        title: "Book Three", 
+        author: "Author C", 
+        review: "Very informative." 
+      },
+      { 
+        isbn: "44556", 
+        title: "Book Four", 
+        author: "Author D", 
+        review: "Not what I expected." 
+      },
+      { 
+        isbn: "78901", 
+        title: "Book Five", 
+        author: "Author E", 
+        review: "An amazing adventure!" 
+      },
+      { 
+        isbn: "23456", 
+        title: "Book Six", 
+        author: "Author F", 
+        review: "A bit slow, but decent." 
+      },
+      { 
+        isbn: "34567", 
+        title: "Book Seven", 
+        author: "Author G", 
+        review: "Engaging from start to finish." 
+      },
+      { 
+        isbn: "45678", 
+        title: "Book Eight", 
+        author: "Author H", 
+        review: "Could have been better." 
+      },
+      { 
+        isbn: "56789", 
+        title: "Book Nine", 
+        author: "Author I", 
+        review: "Great for beginners." 
+      },
+      { 
+        isbn: "67812", 
+        title: "Book Ten", 
+        author: "Author J", 
+        review: "A masterpiece in writing." 
+      },
+      { 
+        isbn: "23467", 
+        title: "Book Eleven", 
+        author: "Author K", 
+        review: "An emotional journey." 
+      },
+      { 
+        isbn: "56780", 
+        title: "Book Twelve", 
+        author: "Author L", 
+        review: "A perfect mystery novel." 
+      }
 ];
+    
+
